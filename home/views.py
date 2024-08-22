2503ea9a3e8fb0518551cbb801c8f188d04fa812
tkanhhh <trinhkieuanh17@gmail.com>
2024-08-23 02:32:23 +0700

    4. template

--- home/views.py
+++ home/views.py
@@ -1,9 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 def index(request):
-   response = HttpResponse()
-   response.writelines('<h1>Hallo</h1>')
-   response.write('This is my new blog')
-   return response
+   return render(request, 'pages/home.html')

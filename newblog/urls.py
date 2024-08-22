0905f082482258aff6b21883b69e25a607c38000
tkanhhh <trinhkieuanh17@gmail.com>
2024-08-23 03:34:39 +0700

    13. 404

--- newblog/urls.py
+++ newblog/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('blog/', include('blog.urls')),
 ]
+
+handler404 = 'home.views.error'
